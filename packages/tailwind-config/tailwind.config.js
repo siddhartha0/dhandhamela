65e3c311f9a4bb653601434f768da10ea981ec0c
siddhartha0 <siddhartha.sunuwar@gmail.com>
2024-09-30 09:31:46 +0545

    tailwind done

--- packages/tailwind-config/tailwind.config.js
+++ packages/tailwind-config/tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+export default {
+  content: [
+    "./src/**/*.{html,js,ts,tsx}",
+    "./app/**/*.{html,js,ts,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     colors: {
       brand: "#235784",
